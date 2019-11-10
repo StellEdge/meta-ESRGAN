2c2f4bd90b6dddd51e14eb6fb66ff18b8d5d270c
StellEdge <1030110910@qq.com>
2019-11-10 21:23:04 +0800

    add RRDB net

--- meta-ESRGAN/RRDB.py
+++ meta-ESRGAN/RRDB.py
@@ -42,3 +42,45 @@ class RRDB(nn.Module):
             out=self.blocks[i](out)
         return out * res_alpha + x
 
+class RRDBNet(nn.Module):
+    def __init__(self, channel_in, channel_out, channel_flow, block_num, gc=32):
+        super(RRDBNet, self).__init__()
+        RRDB_block_f = functools.partial(RRDB, nf=channel_flow, gc=gc)
+
+        self.conv_first = nn.Conv2d(channel_in, channel_flow, 3, 1, 1, bias=True)
+        self.RRDB_trunk = make_layer(RRDB_block_f, block_num)
+        self.trunk_conv = nn.Conv2d(channel_flow, channel_flow, 3, 1, 1, bias=True)
+        #### upsampling
+        self.upsample=[
+            F.interpolate(fea, scale_factor=2, mode='nearest'),
+            nn.Conv2d(channel_flow, channel_flow, 3, 1, 1, bias=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            F.interpolate(fea, scale_factor=2, mode='nearest'),
+            nn.Conv2d(channel_flow, channel_flow, 3, 1, 1, bias=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Conv2d(channel_flow, channel_flow, 3, 1, 1, bias=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Conv2d(channel_flow, channel_out, 3, 1, 1, bias=True)
+        ]
+
+        '''
+        self.upconv1 = nn.Conv2d(channel_flow, channel_flow, 3, 1, 1, bias=True)
+        self.upconv2 = nn.Conv2d(channel_flow, channel_flow, 3, 1, 1, bias=True)
+        self.HRconv = nn.Conv2d(channel_flow, channel_flow, 3, 1, 1, bias=True)
+        self.conv_last = nn.Conv2d(channel_flow, channel_out, 3, 1, 1, bias=True)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        '''
+
+    def forward(self, x):
+        fea = self.conv_first(x)
+        trunk = self.trunk_conv(self.RRDB_trunk(fea))
+        fea = fea + trunk
+
+        out=self.upsample(fea)
+        '''
+        fea = self.lrelu(self.upconv1(F.interpolate(fea, scale_factor=2, mode='nearest')))
+        fea = self.lrelu(self.upconv2(F.interpolate(fea, scale_factor=2, mode='nearest')))
+        out = self.conv_last(self.lrelu(self.HRconv(fea)))
+        '''
+        return out
