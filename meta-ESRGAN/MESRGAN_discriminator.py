9f5992dbed2aef2e6eac2ac3f236adca9fbab2b0
StellEdge <1030110910@qq.com>
2019-11-16 14:58:31 +0800

    discriminator complete

--- meta-ESRGAN/MESRGAN_discriminator.py
+++ meta-ESRGAN/MESRGAN_discriminator.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torchvision
 
+#from xinntao
 class VGGFeatureExtractor(nn.Module):
     def __init__(self, feature_layer=34, use_bn=False, use_input_norm=True,
                  device=torch.device('cpu')):
@@ -33,3 +34,55 @@ class VGGFeatureExtractor(nn.Module):
             x = (x - self.mean) / self.std
         output = self.features(x)
         return output
+
+
+class discriminator_VGG(nn.module):
+    def __init__(self,channel_in,channel_gain,input_size):
+        super(discriminator_VGG,self).__init__()
+
+        #first conv
+        self.in_conv=[nn.Conv2d(channel_in,channel_gain,3,1,1,bias=True),
+                      nn.LeakyReLU(0.2,True),
+                      nn.Conv2d(channel_gain,channel_gain,4,2,1,bias=False),
+                      nn.BatchNorm2d(channel_gain,affine=True),
+                      nn.LeakyReLU(0.2,True),
+                      ]
+        cur_dim=channel_gain
+        cur_size=input_size/2
+        self.conv_layers=[]
+        for i in range(3):
+            conv_layers+=build_conv_block(cur_dim)
+            cur_dim*=2
+            cur_size/=2
+        self.out_conv=[
+            nn.Conv2d(cur_dim,cur_dim,3,1,1,bias=True),
+            nn.BatchNorm2d(cur_dim,affine=True),
+            nn.LeakyReLU(0.2,True),
+            nn.Conv2d(cur_dim,cur_dim,4,2,1,bias=True),
+            nn.BatchNorm2d(cur_dim,affine=True),
+            nn.LeakyReLU(0.2,True),
+            ]
+        cur_size/=2
+
+        self.linear =[nn.Linear(cur_dim * cur_size * cur_size, 100),
+                      nn.LeakyReLU(0.2,True),
+                      nn.Linear(100, 1)
+                      ]
+
+    def build_conv_block(self,channel_gain):
+        model=[
+            nn.Conv2d(channel_gain,channel_gain*2,3,1,1,bias=True),
+            nn.BatchNorm2d(channel_gain*2,affine=True),
+            nn.LeakyReLU(0.2,True),
+            nn.Conv2d(channel_gain*2,channel_gain*2,4,2,1,bias=True),
+            nn.BatchNorm2d(channel_gain*2,affine=True),
+            nn.LeakyReLU(0.2,True),
+            ]
+        return model
+    def forward(self,x):
+        x=self.in_conv(x)
+        x=self.conv_layers(x)
+        x=self.out_conv(x)
+        x=x.view(x.size(0),-1)
+        out=self.linear(x)
+        return out
--- meta-ESRGAN/meta_ESRGAN_train.py
+++ meta-ESRGAN/meta_ESRGAN_train.py
@@ -1,1 +1,4 @@
+'''
+TRain MESRGAN with DIV2K https://data.vision.ee.ethz.ch/cvl/DIV2K/
+'''
 
